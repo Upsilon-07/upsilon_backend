382cc6a978429666bf5c92b2fcafcdf3de4ff3d0
Vanessa Dallaris <dallarisvanessa@yahoo.com>
2023-08-31 12:22:14 +0200

    Remove comment in course-routes

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -1,7 +1,7 @@
 
 const courseRouter = require("express").Router()
 const CourseController = require("../controllers/course.controller")
-//endpoint:/courses
+
 courseRouter.get("/",CourseController.getAllCourses)
 
 module.exports = courseRouter
