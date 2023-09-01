0b0ab426844fb6b097ed5b73b5467fe164e825d8
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-09-01 09:55:25 +0100

    Removed endpoint /auth/sign-up
    Removed endpoint /auth/sign-up

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,9 @@
-const authRoutes = require("./auth.routes")
+
 // import routes
 const courseRouter = require("./course.routes")
 
 const setupRoutes = (server) => {
 //   server.use("<endpoint>", <route>);
-    server.use("/auth", authRoutes);
 server.use("/courses",courseRouter);
 };
 
