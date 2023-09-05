61b0aa6286c48270b0e783ecf99b1f359019b7f3
Vanessa Dallaris <dallarisvanessa@yahoo.com>
2023-09-05 17:02:30 +0200

    Co-authored-by: Luis Clara <LuisC92@users.noreply.github.com>

--- src/routes/favourites.routes.js
+++ src/routes/favourites.routes.js
@@ -3,10 +3,11 @@ const favouritesRouter = require("express").Router()
 const CourseController = require("../controllers/course.controller")
 
 //* /favourites/courses
+//endpoint:/favourites/courses
 favouritesRouter.post("/courses",CourseController.postIsFavourite)
 
 //* /favourites/meals
-// favouritesRouter.post("/meals",CourseController.postIsFavourite)
+// favouritesRouter.post("/meals",MealController.postIsFavourite)
 
 
 
