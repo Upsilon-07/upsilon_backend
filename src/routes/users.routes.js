39c6432cff539b84b9226230c698dc8d36b94b54
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-09-08 12:44:20 +0100

    Updated userController in users.routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,12 @@
 const usersRoutes = require("express").Router();
-const UserController = require("../controllers/users.controller");
+const userController = require("../controllers/users.controller");
 const { verifyToken } = require("../middlewares/users.middleware");
 
-usersRoutes.get("/", verifyToken, UserController.getUserInfo);
+usersRoutes.get("/", verifyToken, userController.getUserInfo);
 
 
 // Update user information
 //* endpoint: /user/:id
-usersRoutes.put("/:id", UserController.editUserInfo)
+usersRoutes.put("/:id", userController.editUserInfo)
 
 module.exports = usersRoutes;
