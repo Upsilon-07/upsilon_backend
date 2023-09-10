cd09adbc68c640488386d4198c505bd740302e37
Laura Carvalho <115020549+lauratlcarvalho@users.noreply.github.com>
2023-09-10 18:48:56 +0100

    Created route, controller and model for meals
    Created routes, controller and model to get all meals and meals by id information from DB.

--- src/models/meal.model.js
+++ src/models/meal.model.js
@@ -3,9 +3,25 @@ const database = require("../database/database-config");
 const getall = () => {
   return database
     .query("SELECT * FROM meals")
-    .then(([results]) => console.log(results));
+    .then(([results]) => results);
 };
+
+const getMealById = (id) => {
+  // console.log("id", id);
+  return database
+  .query("SELECT * FROM meals WHERE id=?", id)
+  .then(([results]) => results);
+};
+
+// const getMealByMealType = (mealType) => {
+//   // console.log("id", id);
+//   return database
+//   .query("SELECT * FROM meals WHERE meal_type=?", mealType)
+//   .then(([results]) => console.log(results));
+// };
 
 module.exports = {
   getall,
+  getMealById,
+  // getMealByMealType,
 };
--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mealRouter = express.Router()
-const MealController = require("../controllers/meal.controller")
+const mealRouter = express.Router();
+const MealController = require("../controllers/meal.controller");
 
 //* get all meals
 //* endpoint: /meals
@@ -8,6 +8,15 @@ mealRouter.get("/", MealController.getAllMeals)
 
 
 //* get all meals by id
+//* endpoint: /meals/:id
+mealRouter.get("/:id", MealController.getMealbyId)
+
+
+// //* get all meals by meal_type
+// //* endpoint: /meals/:meal_type
+// mealRouter.get("/:meal_type", MealController.getMealByMealType)
+
+
 
 
 module.exports = mealRouter
